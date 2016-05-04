73fb05420abc56aa9c922e1469b1c5e2bee8f9bc
jwlehman93 <jeremylehman9@gmail.com>
2016-05-04 00:39:11 -0400

    initial

--- samples/companionService/config.js
+++ samples/companionService/config.js
@@ -14,17 +14,17 @@
  * not need to modify those elements.
  */
 var config = {
-    clientId: '',
-    clientSecret: '',
+    clientId: 'amzn1.application-oa2-client.334cb745be3f4d85849ee18ff8a6f3c4',
+    clientSecret: '2cc78af962e9801170abdf4507e042038ebc9cc683c0b9773c42c474169960f5',
     redirectUrl: 'https://localhost:3000/authresponse',
     lwaRedirectHost: 'amazon.com',
     lwaApiHost: 'api.amazon.com',
     validateCertChain: true,
-    sslKey: '',
-    sslCert: '',
-    sslCaCert: '',
+    sslKey: '/home/alarm/alexa/samples/javaclient/certs/server/node.key',
+    sslCert: '/home/alarm/alexa/samples/javaclient/certs/server/node.crt',
+    sslCaCert: '/home/alarm/alexa/samples/javaclient/certs/ca/ca.crt',
     products: {
-        "": [""], // Fill in with valid device values, eg: "testdevice1": ["DSN1234", "DSN5678"]
+        "alexa_pi": ["123456"], // Fill in with valid device values, eg: "testdevice1": ["DSN1234", "DSN5678"]
     },
 };
 
